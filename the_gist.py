4a24c06040e03043022ab85ca14369dfe2d524b1
Don Johnson <don.johnson@j2.com>
2017-11-28 13:34:59 -0800

    wip

--- the_gist.py
+++ the_gist.py
@@ -38,20 +38,23 @@ def fetch_all_processed_files():
 def collect_files(root_directory, file_mask):
     """ a recursive method to collect a list of files
        in a given root directory"""
-    processed = fetch_all_processed_files()
+    proceesed = fetch_all_processed_files()
     matches = []
-    print "Proccessed: {}".format(processed)
 
     for root, dirnames, filenames in os.walk(root_directory):
         for filename in fnmatch.filter(filenames, file_mask):
-            if "_" in filename and any(processed) not in filename:
-                matches.append(os.path.join(root, filename))
-                insert_record_as_processed(filename)
-            else:
-                pass
+            insert_record_as_processed(filename)
+            for p in proceesed:
+                boold = p in filename
+                if boold is True:
+                    pass
+                elif "_" in filename:
+                    matches.append(os.path.join(root, filename))
+                else:
+                    pass
     return matches
 
 
 
-
-a = collect_files('test_data/','*.txt')
+a = collect_files("test_data/","*.txt")
+print a
